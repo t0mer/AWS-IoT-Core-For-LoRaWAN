543507f4ecf40a6b440f2da8c2b6fd9cf6ee6664
Tomer Klein <tomer.klein@gmail.com>
2024-01-25 09:14:13 +0200

    Create get_device_info.py

---
+++ Devices/get_device_info.py
@@ -0,0 +1,13 @@
+import boto3, json
+from loguru import logger
+
+session = boto3.Session(profile_name='', region_name='eu-west-1')
+client = session.client('iotwireless')
+
+response = client.get_wireless_device(
+    Identifier='',
+    IdentifierType='DevEui'
+)
+
+print(response["Id"])
+
